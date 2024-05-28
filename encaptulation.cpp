1910f1854a886af952d1467fffa74090da7603a3
aryadamara <aryadamara@DESKTOP-PT9UTUQ>
2024-05-28 15:47:59 +0700

    #include <iostream> using namespace std;
    class remoteLampu {
        private:
        string saklarNo[10];
        public:
        void setSaklarNo(int i, string value) {
            saklarNo[i] = value;
        }
        string getSaklarNo(int i) {
            return saklarNo[i];
        }
    };

---
+++ encaptulation.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class remoteLampu {
+    private:
+    string saklarNo[10];
+    public:
+    void setSaklarNo(int i, string value) {
+        saklarNo[i] = value;
+    }
+    string getSaklarNo(int i) {
+        return saklarNo[i];
+    }
+};
+
+int main() {
+    remoteLampu lampuRumah;
+
+    lampurumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampurumah.setSaklarNo(1, "Lampu Ruag Tamu");
+    lampurumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampurumah.setSaklarNo(3, "Lampu Dapur");
+
+    cout << lampuRumah.getSaklarNo(0) << endl;
+    cout << lampuRumah.getSaklarNo(1) << endl;
+    cout << lampuRumah.getSaklarNo(2) << endl;
+    cout << lampuRumah.getSaklarNo(3) << endl;
+    return 0;
+}
